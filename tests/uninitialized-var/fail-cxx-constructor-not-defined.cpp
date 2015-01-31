b1a2ffa96649adc4dae3c3be4d3601a14eddfe60
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-01-31 22:51:17 +0100

    crbug 411177 and an earlier test edited

--- tests/uninitialized-var/fail-cxx-constructor-not-defined.cpp
+++ tests/uninitialized-var/fail-cxx-constructor-not-defined.cpp
@@ -1,16 +1,11 @@
 /* This file tests if clang analyzer flags
  * uninitialized use of member fields of cxx objects in function calls when the
- * constructor of the object is declared but not defined. Note that this is
- * an unrealistic test case.
+ * constructor of the object is declared but not defined.
+ * Note: It is possible that the definition of constructor is in a different source
+ * file and this file simply makes use of class foo.
  * Result: Fail
 */
-
-class foo
-{
-public:
-        int x;
-        foo();
-};
+#include "include/cxx-obj-declaration.h"
 
 void call(int a) {}
 
---
+++ tests/uninitialized-var/fail-cxx-uninstantiated-object-uninitialized-field.cpp
@@ -0,0 +1,28 @@
+/* crbug.com/411177
+ * This file tests if clang analyzer flags a warning in the following scenario
+ * a) Constructor fails to initialize member field
+ * b) member field is defined in one member function
+ * c) same member field is used in another member function
+ * Developers might assume a certain call order between member functions of a
+ * class and overlook lack of initialization. This is not concocted but a real
+ * bug from Chromium source code. Read the bug report and patches.
+ * Result: Fail
+*/
+
+class foo {
+public:
+        foo() {}
+        void init_member();
+	void read_member();
+        int m_x;
+};
+
+void foo::init_member() {
+	m_x = 0;
+}
+
+void foo::read_member() {
+	if(!m_x) 		// No warning!
+		m_x = 10;
+}
+
