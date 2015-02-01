754060241f071d6cb29eac3d7a74556022e6c6c4
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-02-01 14:36:27 +0100

    crbug 419328: commented test files thoroughly; minor change in fail-cxx-struct-field-uninitialized.cpp

--- tests/uninitialized-var/fail-cxx-calls-across-object-definitions.cpp
+++ tests/uninitialized-var/fail-cxx-calls-across-object-definitions.cpp
@@ -1,9 +1,18 @@
+/* crbug.com/419428
+ * This test is a simplified version of crbug 419428
+ * Essentially, it tests if calls across class definitions
+ * is sufficient to confuse clang. tl;dr: yes, it is!
+ * Result: Fail
+*/
+
 #include "include/cxx-calls-across-object-definitions.h"
 
 void bar::call(bool cond) {
+	/* isTrue is initialized if cond is true */
 	if(cond)
 		fooInstance.isTrue = true;
 
-	fooInstance.updateX();
+	/* Member field isTrue is read in function updateX() */
+	fooInstance.updateX(); // No warning!
 	return;
 }
--- tests/uninitialized-var/fail-cxx-struct-field-uninitialized.cpp
+++ tests/uninitialized-var/fail-cxx-struct-field-uninitialized.cpp
@@ -1,10 +1,33 @@
+/* crbug.com/419428
+   This file tests if clang flags a warning in the following scenario:
+ * a) There are two distinct cxx object definitions available
+ * b) One object (bar) has an object of the other type (foo)
+ * c) Object bar has a member function that calls a member function of foo
+ *    through its own instance of foo (fooInstance)
+ * d) Member field of foo is uninitialized when a condition (cond) is false
+ * Result: Fail
+*/
+
 #include "include/cxx-struct-field-uninitialized.h"
 
+/* crbug 419428: bar is equivalent to class MediaQueryData.
+ * bar::call is equivalent to MediaQueryData::addParserValue()
+ */
 void bar::call(bool cond) {
+
+	/* crbug 419428: foostruct is equivalent to struct CSSParserValue */
 	foostruct fs;
-	foo fooobj;
 	if(cond)
 		fs.isTrue = true;
-	fooobj.addFooStructInstance(fs);
+
+	/* If cond == false, the branch is not taken
+	 * and fs.isTrue is not initialized.
+	 * Clang is expected to flag a warning here.
+	*/
+	/* crbug 419428: foo is equivalent to class CSSParserValueList.
+	 * fooInstance is equivalent to field m_valueList in MediaQueryData.
+	 * addFooStructInstance() is equivalent to CSSParserValueList::addValue().
+	 */
+	fooInstance.addFooStructInstance(fs); // No warning!
 	return;
 }
