fd3bb8a94b3d1f3492abafc2cabbcefb1fe32571
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-02-02 12:57:39 +0100

    More commentary on test file

--- tests/uninitialized-var/fail-cxx-calls-across-object-definitions.cpp
+++ tests/uninitialized-var/fail-cxx-calls-across-object-definitions.cpp
@@ -16,3 +16,17 @@ void bar::call(bool cond) {
 	fooInstance.updateX(); // No warning!
 	return;
 }
+
+/* This is what is missing for clang analyzer to flag a warning.
+ * Without this function that actually instantiates object of
+ * type `bar`, the analyzer doesn't do anything interesting
+ * Uncomment to see clang flag a warning
+ */
+/*
+void func(bool cond) {
+	bar b;
+	// warning: Branch condition evaluates to a garbage value in
+	// include/cxx-calls-across-object-definitions.h:17:5
+	b.call(cond);
+}
+*/
