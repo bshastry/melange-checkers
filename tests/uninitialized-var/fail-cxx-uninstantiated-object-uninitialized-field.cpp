23c1a18cb9b140ab3700fc1302002a54097a487a
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-02-12 02:09:26 +0100

    Modified test case, We have a fail test case that our checker passes if only in a conjured scenario

--- tests/uninitialized-var/fail-cxx-uninstantiated-object-uninitialized-field.cpp
+++ tests/uninitialized-var/fail-cxx-uninstantiated-object-uninitialized-field.cpp
@@ -7,6 +7,14 @@
  * class and overlook lack of initialization. This is not concocted but a real
  * bug from Chromium source code. Read the bug report and patches.
  * Result: Fail
+ * ----------------------------
+ * Update: 12.02.15
+ * Checker-v2.0 flags a sub-set of undefined use of cxx object fields
+ * a) Definition in ctor initializer, in-class, or in method definition is
+ *	recorded
+ *	TODO: Record definition in ctor body as well
+ * b) Use is checked for binary operator ``=" i.e., being on the rhs
+ *    of assignment qualifies as use
 */
 
 class foo {
@@ -15,14 +23,17 @@ public:
         void init_member();
 	void read_member();
         int m_x;
+	int m_y;
 };
 
 void foo::init_member() {
-	m_x = 0;
+	/* Clang SA: No warning here
+	 * Our checker flags this
+	 */
+	m_x = m_y;
 }
 
 void foo::read_member() {
-	if(!m_x) 		// No warning!
+	if(!m_x) 		// Clang SA: No warning!
 		m_x = 10;
 }
-
