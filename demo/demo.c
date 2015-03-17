4714b6ab29bd914b458cf8a0a3cfdb5744e1afa6
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-03-17 17:22:15 +0100

    Cleaning up demo folder which has nothing interesting.

--- demo/demo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello checker!\n");
-	int x = 0;
-	x += 1;
-	return x;
-}
-
--- demo/demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LLVM_SOURCE=$HOME/work/clang-analyzer/git-mirror-test/llvm
-LLVM_BIN=$HOME/workspace/llvm/bin
-
-make clean
-sbuild=$LLVM_SOURCE/tools/clang/tools/scan-build/scan-build
-$sbuild --use-analyzer $LLVM_BIN/clang -load-plugin $1 -enable-checker $2 make
