ad4c9517b8d9652e7deea8494b1c432eda926797
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-10-01 18:41:13 +0200

    TypeCastingVulnChecker: First version that flags unsafe C style casts like so int a; unsigned u = (unsigned)a;

--- src/TypeCastingVulnChecker.cpp
+++ src/TypeCastingVulnChecker.cpp
@@ -6,5 +6,62 @@ using namespace Melange;
 
 void TypeCastingVulnChecker::checkPreStmt(const ExplicitCastExpr *ECE, CheckerContext &C) const {
 
+  const auto *CE = static_cast<const CastExpr *>(ECE);
+  const auto *castSE = CE->getSubExpr();
+  const auto *ICE = dyn_cast<ImplicitCastExpr>(castSE);
 
+  if (!ICE)
+    return;
+
+  DEBUG_PRINT("Is implicit cast");
+
+  // Implicitcastexpr
+
+  if (ICE->getCastKind() != CK_LValueToRValue)
+    return;
+
+  DEBUG_PRINT("Involves lvaltorval conv");
+
+  // lvaltorval
+
+  const auto *castCore = castSE->IgnoreParenImpCasts();
+  const auto *castDRE = dyn_cast<DeclRefExpr>(castCore);
+  if (!castDRE)
+      return;
+
+  DEBUG_PRINT("castee is declrefexpr");
+  // declrefexpr
+  const auto *VD = castDRE->getDecl();
+  auto castFromType = VD->getType();
+  auto castToType = ECE->getTypeAsWritten();
+
+  if (castFromType == castToType)
+    return;
+
+  DEBUG_PRINT("castfrom type: " + castFromType.getAsString());
+  DEBUG_PRINT("castto type: " + castToType.getAsString());
+
+  std::string Message = "Cast From " + castFromType.getAsString() + " to " +
+			castToType.getAsString() + " may be unsafe.";
+
+  // Report bug
+  reportBug(C, ECE->getSourceRange(), Message);
+}
+
+void TypeCastingVulnChecker::reportBug(CheckerContext &C, SourceRange SR,
+                                       StringRef Message) const {
+  const char *name = "Type casting vulnerability checker";
+  const char *desc = "Flags potential unsafe casts";
+
+  ExplodedNode *EN = C.generateSink();
+  if (!EN)
+    return;
+
+  if (!BT)
+    BT.reset(new BuiltinBug(this, name, desc));
+
+  BugReport *R = new BugReport(*BT, Message, EN);
+  R->addRange(SR);
+
+  C.emitReport(R);
 }
--- src/TypeCastingVulnChecker.h
+++ src/TypeCastingVulnChecker.h
@@ -2,12 +2,24 @@
 #define MELANGE_TYPECASTINGVULN_CHECKER_H
 
 #include "clang/StaticAnalyzer/Core/Checker.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+#include "clang/StaticAnalyzer/Core/BugReporter/BugType.h"
+
+#ifdef _DEBUG
+#define DEBUG_PRINT(x) llvm::errs() << x << "\n"
+#else
+#define DEBUG_PRINT(x)
+#endif
 
 namespace Melange {
 
 class TypeCastingVulnChecker : public clang::ento::Checker<clang::ento::check::PreStmt<clang::ExplicitCastExpr>> {
 public:
   void checkPreStmt(const clang::ExplicitCastExpr *ECE, clang::ento::CheckerContext &C) const;
+private:
+  mutable std::unique_ptr<clang::ento::BugType> BT;
+  void reportBug(clang::ento::CheckerContext &C, clang::SourceRange SR,
+                 llvm::StringRef Message) const;
 };
 } // end of Melange namespace
 
